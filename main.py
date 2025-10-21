013b16453f1b24ad0a04e9a917184c4bdd493bf4
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-10-21 14:09:39 +0300

    added password draft code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello GitHub, it works!")
+from string import punctuation
+
+def add_password(web, user, password):
+    website = input("Enter website name: ")
+    url = f"{website}.com"
+
+    user = input("Enter username: ")
+    
+    password = input("Enter password: ")
+    while True:
+        if len(password) < 5:
+            print("The length of password is less than 5 characters.")
+        
+            print("Password is not strong enough, include a special character.")
+        else:
+            print("Password is strong enough.")
+            break  
