d5faee86fa17662c160014ec91b00d597da8c8e7
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-10-21 14:20:54 +0300

    refactor password validation to check for special characters

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ from string import punctuation
 def add_password(web, user, password):
     website = input("Enter website name: ")
     url = f"{website}.com"
-
     user = input("Enter username: ")
     special_chars = '!@#$%^&*()-+_=,'
-    
     password = input("Enter password: ")
     while True:
         if len(password) < 5:
             print("The length of password is less than 5 characters.")
         
-            print("Password is not strong enough, include a special character.")
-        else:
-            print("Password is strong enough.")
-            break  
+        elif  any(char not in special_chars for char in password):
+            print("Password does not include speical characters")
+
+        
+
+          
+    
