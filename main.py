4ecbfae68fbf0073dd2fd540e7c2801726d8d52c
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-10-21 17:48:46 +0300

    refactor password validation loop for improved readability and structure

--- main.py
+++ main.py
@@ -3,22 +3,28 @@ def add_password(web, user, password):
     url = f"{website}.com"
     user = input("Enter username: ")
     special_chars = '!@#$%^&*()-+_=,'
-    
+
     password = input("Enter password: ")
     while True:
         if len(password) < 5:
             print("The length of password is less than 5 characters.")
-        
+            continue
         elif not any(char.lower() for char in password):
             print("Password does not include any lowercase letters")
-        
+            continue
         elif  not any(char in special_chars for char in password):
             print("Password does not include speical characters")
-        
+            continue
         elif not any(str.isdigit() for char in password):
             print("Password does not include any number")
         else:
             print("Password is strong enough")
+        break
+
+    
+
+        
+        
         
         
         
