905ab57c473cb2688facbc37ede34c16848cb2ac
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-10-28 15:42:40 +0300

    refactor password generator function for improved structure and readability

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#import numpy as np
+import random as ran
+#import secrets as sec
+from urllib import request
 # def website():
 #     website = input("Enter website name: ") 
 #     url = f"{website}.com"
@@ -5,18 +9,29 @@
 
 def password_generator():
     
-    choice = input("Would you like to generate a random password? ")
-    if choice == "yes":
-        return True
+    #choice = input("Would you like to generate a random password? ")
+    #if choice == "yes":
+        
+        word_site = "https://www.mit.edu/~ecprice/wordlist.10000" # A word site from the internet 
+        response = request.urlopen(word_site) # The reponse requests the url to open from word site
+        txt = response.read() # downloads the webpage data in bytes from the URL
+        words = txt.decode().splitlines() #converts the bytes into readable texts then splits into one word
+        random_word = ran.choice(words)
+        random
+
+    
+        
+
+       
         
        
-    elif choice == "no":
-        return False
+    # elif choice == "no":
+    #     return False
     
         
 
 
-    
+"""""
 def strength_password():
     generator_choice = password_generator()
     
@@ -43,12 +58,13 @@ def strength_password():
         else:
             print("Password is strong enough")
         break
+"""""
 
 
 
 
-
-strength_password()
+#strength_password()
+password_generator()
 
 
 
