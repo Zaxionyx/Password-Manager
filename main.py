ad2cb91e97e886d614ae73d1b74d3fe3116ec678
Adnan Al-Hmoud <adnanalhmoud@outlook.com>
2025-10-28 18:40:24 +0300

    refactor password_generator function for improved structure and readability , A bug has been discovered...

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
 #import numpy as np
 import random as ran
 #import secrets as sec
-from urllib import request
+import string
+ 
 # def website():
 #     website = input("Enter website name: ") 
 #     url = f"{website}.com"
-    
 
 def password_generator():
     
-    #choice = input("Would you like to generate a random password? ")
-    #if choice == "yes":
-        
-        word_site = "https://www.mit.edu/~ecprice/wordlist.10000" # A word site from the internet 
-        response = request.urlopen(word_site) # The reponse requests the url to open from word site
-        txt = response.read() # downloads the webpage data in bytes from the URL
-        words = txt.decode().splitlines() #converts the bytes into readable texts then splits into one word
-        random_word = ran.choice(words)
-        number = ran.randint(100,9999)
-        
-        print(random_word + str(number)) # A future edit I will find a way to randomly generate this instead of it being orderded
-
-
-
-    
-        
-
-       
-        
-       
-    # elif choice == "no":
-    #     return False
-    
-        
-
-
-"""""
+    random_string = ""
+    while True:
+        choice = input("Do you want to generate a password? ")
+        if choice == "yes":
+            string_length = int(input("Random String Length: "))
+            if string_length >= 5:
+                possible_characters = input("Possible Characters: ")
+                random_string += ran.choice(possible_characters)
+                print(f"Generated password: {string_length, possible_characters}")
+                break
+        elif choice == "no":
+         return password_generator()             
+                   
 def strength_password():
     generator_choice = password_generator()
     
@@ -62,13 +48,6 @@ def strength_password():
         else:
             print("Password is strong enough")
         break
-"""""
-
-
-
-
-#strength_password()
-password_generator()
 
 
 
@@ -87,12 +66,12 @@ password_generator()
 
 
 
-# def store():
-#     txt_data = add_password()
-#     file_path = "pwd0.txt"
 
-#     with open(file_path, "r") as file: 
-#         pass 
+
+
+
+
+
 
     
 
